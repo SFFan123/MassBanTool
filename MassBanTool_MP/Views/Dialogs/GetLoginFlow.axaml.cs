b1ff94bb154080fbb94e2f06ec15c3692cc75604
SFFan123 <38184195+SFFan123@users.noreply.github.com>
2023-02-15 21:47:48 +0100

    Cleanup

--- MassBanTool_MP/Views/Dialogs/GetLoginFlow.axaml.cs
+++ MassBanTool_MP/Views/Dialogs/GetLoginFlow.axaml.cs
@@ -4,7 +4,6 @@ using System.Web;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using MassBanToolMP.ViewModels;
-using Microsoft.CodeAnalysis.FlowAnalysis;
 using DialogResult = MassBanToolMP.Helper.DialogResult;
 
 namespace MassBanToolMP.Views.Dialogs
