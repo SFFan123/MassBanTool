bcddb4ed3759e8f62836717205bbd6ae0258aa40
SFFan123 <38184195+SFFan123@users.noreply.github.com>
2023-02-18 15:42:30 +0100

    Show delay change

--- MassBanTool_MP/ViewModels/MainWindowViewModel.cs
+++ MassBanTool_MP/ViewModels/MainWindowViewModel.cs
@@ -1581,6 +1581,7 @@ namespace MassBanToolMP.ViewModels
                     if (increaseDelay)
                     {
                         _messageDelay++;
+                        RaisePropertyChanged(nameof(MessageDelay));
                         increaseDelay = false;
                     }
                     
